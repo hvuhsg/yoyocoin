5a9883a9913259b6d7ee7a23c9362259684890bc
hvuhsg5@gmail.com <hvuhsg6@gmail.com>
2021-08-01 17:04:00 +0300

    make winner score bigger (fix privies winner is the lowest score)

--- src/blockchain/consensus/lottery.py
+++ src/blockchain/consensus/lottery.py
@@ -26,8 +26,8 @@ def find_lottery_winner(root: SumTree, lottery_number: float) -> int:
 
 
 def wallet_distance(winner_index: int, wallet_index: int, wallets_count: int):
-    # The 0.1 is added fo tie break
-    return min(
+    # The 0.1 is added for tie break
+    return 100 - min(
         abs(winner_index - wallet_index) + 0.1,
         winner_index + (wallets_count - wallet_index),
     )
